e128fc9a4950dec180bb01f13e86cfbef7f54e1a
Christian Anderson <tehninjawaffle@gmail.com>
2023-05-03 07:23:33 -0700

    A slight issue, I didn't realize it had to be specifically named VOLUME, i was naming it vCalc. Took a while to figure that out.

--- index.js
+++ index.js
@@ -210,7 +210,9 @@ function CuboidMaker(obj){
   Create a method called volume using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
   💡 NOTE: Formula for cuboid volume: length * width * height   
 */
-
+CuboidMaker.prototype.volume = function() {
+  return this.length * this.width * this.height;
+}
 
 
 
